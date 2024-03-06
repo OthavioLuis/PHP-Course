3c12ed0c0c299f70cbd89830686fbf6d5b1885b5
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-03-06 12:51:27 -0300

    variaveis e constantes

---
+++ ex003/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e Constantes</title>
+</head>
+<body>
+    <?php 
+    $nome = "Othavio";
+    $sobrenome = "Rodrigues";
+    const PAIS = "Brasil";
+
+    echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+    ?>
+</body>
+</html>
+
