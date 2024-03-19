10fec17c5659fb5b481877234da1d3ecb9c6dc61
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-03-19 16:15:43 -0300

    Aula Tipos de String

---
+++ ex004/strings.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de String</title>
+</head>
+<body>
+    <h1>Tipos de String</h1>
+    <?php 
+        $nome = "Gustavo";
+        $sobrenome = "Guanabara";
+        //não importa se as variaveis estão com single ou double aspas
+        echo "$nome $sobrenome"; //com aspas duplas
+        echo ' e $nome $sobrenome'; //com aspas simples
+    ?>
+</body>
+</html>
