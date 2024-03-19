a58cc2294090f78cb691df7aafdc70832d89511d
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-03-19 15:51:39 -0300

    descrições da aula de tipos de variaveis

---
+++ ex004/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+
+    // 0x = hexadecimal
+    // 0b = binário
+    // 0 = Octal
+    //     $num = 010;
+    //     echo "O valor da variável é $num";
+
+    // $v = "Othavio";
+    // var_dump($v);
+
+    //$num = 3e2; // 3 x 10 elevado a 2 - resultado 300
+    //echo "O valor é $num";
+
+    // $num = (int) 3e2;
+    // var_dump($num);
+
+    // $num = (float) "950"; //caso queira mudar, (digite o tipo que vc quer que vire a variável)
+    // var_dump($num);
+
+    ?>
+</body>
+</html>
