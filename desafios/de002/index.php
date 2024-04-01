95e54b49ca49bb14a94bc5f27f2f267f73b92648
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-04-01 16:53:10 -0300

    adição de comentarios necessarios sobre numeros aleatórios.

--- desafios/de002/index.php
+++ desafios/de002/index.php
@@ -11,7 +11,13 @@
         <h1>Números aleatórios</h1>
         <p>Gerando um número aleatório entre 0 e 100....</p>
         <?php
-            $valor = rand(0, 100);
+
+            //rand() = 1951 - Linear Congrential Generator
+            //mt_rand() = 1997 - Mersenne Twister
+            //A partir do PHP 7.1, rand() é um simples apontamento para mt_rand()
+            //randm_int() gera números aleatórios criptograficamente seguros
+
+            $valor = mt_rand(0, 100);
             echo "<p>O valor gerado foi <strong>$valor</strong></p>"
         ?>
         <input type="button" value="Gerar outro número" onclick="window.location.reload()">
